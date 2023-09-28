985549f924458c059815a3b486f7872da870c051
andrey-pc <anbalabanovkz@gmail.com>
2023-09-28 19:46:48 +0300

    feat(*): fix styles

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -9,15 +9,15 @@ function Footer() {
     const {playing, toggle} = useAudio(radioList.find(({ id }) => id === currentStationId)?.url ?? '');
 
     return (
-        <footer className="fixed inset-x-0 bottom-0">
-                <div className="bg-gray-600 shadow-lg rounded-lg">
+        <footer className="fixed inset-x-0 bottom-0 bg-no-repeat dark:bg-slate-900/75">
+                <div className="shadow-lg rounded-lg">
                     <div className="w-full p-6">
                         <div className="flex justify-between items-center">
-                            <div className="text-gray-100">
-                                <h3 className="text-2xl text-grey-darkest font-medium">A Sky Full of Stars</h3>
-                                <p className="text-sm text-grey mt-1">Ghost Stories</p>
+                            <div className="hover:text-sky-500 dark:hover:text-sky-400">
+                                <h3 className="text-2xl font-medium">A Sky Full of Stars</h3>
+                                <p className="text-sm mt-1">Ghost Stories</p>
                             </div>
-                            <div className="text-gray-100 hover:bg-gray-400 p-8 rounded-full bg-red-light shadow-lg cursor-pointer" onClick={toggle}>
+                            <div className="p-8 rounded-full bg-red-light shadow-lg cursor-pointer hover:text-sky-500 dark:hover:text-sky-400" onClick={toggle}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     {playing ?
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
@@ -26,7 +26,7 @@ function Footer() {
                                     }
                                 </svg>
                             </div>
-                            <div className="text-gray-100" >
+                            <div className="hover:text-sky-500 dark:hover:text-sky-400" >
                                 <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
                                      viewBox="0 0 20 20">
                                     <path
--- app/page.tsx
+++ app/page.tsx
@@ -14,19 +14,24 @@ export default function Home() {
 
     return (
         <main>
-            <div className="bg-white">
+
+            <div className="absolute inset-0 bottom-10 bg-bottom bg-no-repeat bg-slate-50 dark:bg-[#0B1120] index_beams__yWcJT">
+                <div className="mt-20 font-medium py-1 px-2  rounded-full text-gray-700 bg-gray-100 border border-gray-300 ">
+                    <div className="text-xs font-normal leading-none max-w-full flex-initial">Hello!</div>
+                </div>
                 <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                    <h2 className="sr-only">Products</h2>
+
                     <div
                         className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-8 xl:gap-x-8"
                         onClick={handleClick}>
                         {radioList.map(({name, id}) => {
                             return (
                                 <div key={id}
-                                     className={`rounded-lg ${currentStationId === id ? "bg-gray-400" : "bg-gray-200"} hover:bg-gray-400 h-20 w-36 text-center space-x-2 cursor-pointer`}
+                                     className={`rounded-lg ${currentStationId === id ? "dark:bg-indigo-500 bg-gray-200 text-indigo-500 dark:text-slate-300" : "bg-slate-50 dark:bg-slate-800"}  rounded-lg p-6   cursor-pointer`}
                                      data-id={id}
                                 >
                                     {name}
+
                                 </div>
                             )
                         })}
