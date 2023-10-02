73bad0212f58529a3e396cf357a81d3828479820
andrey-pc <anbalabanovkz@gmail.com>
2023-10-02 21:11:07 +0300

    feat(*): fixed footer style

--- app/components/common/station-filter/StationFilter.tsx
+++ app/components/common/station-filter/StationFilter.tsx
@@ -1,32 +1,78 @@
-import React from 'react';
-import {FilterItem} from "@/app/components/common/station-filter/filter-item/FilterItem";
+"use client"
+import React, {useState} from 'react';
 
 const genres = [
-    { id: 1, name: "Поп-музыка"},
-    { id: 2, name: "Разговорное радио"},
-    { id: 3, name: "Рок-музыка"},
-    { id: 4, name: "Электронная музыка"},
-    { id: 5, name: "Танцевальная музыка"},
-    { id: 6, name: "Лёгкая музыка"},
-    { id: 7, name: "Ретро музыка"},
-    { id: 8, name: "Музыка для души"},
-    { id: 9, name: "Шансон"},
-    { id: 10, name: "Классическая музыка"},
-    { id: 11, name: "Рэп и Хип-Хоп"},
-    { id: 12, name: "Русская музыка"},
-    { id: 13, name: "Джаз/Блюз"},
-    { id: 14, name: "Романтика"},
-    { id: 15, name: "Юмор"},
-    { id: 16, name: "Фолк/Этно"},
-    { id: 17, name: "Детская музыка"},
-    { id: 18, name: "Альтернативная музыка/инди"},
+    {id: 1, name: "Поп-музыка"},
+    {id: 2, name: "Разговорное радио"},
+    {id: 3, name: "Рок-музыка"},
+    {id: 4, name: "Электронная музыка"},
+    {id: 5, name: "Танцевальная музыка"},
+    {id: 6, name: "Лёгкая музыка"},
+    {id: 7, name: "Ретро музыка"},
+    {id: 8, name: "Музыка для души"},
+    {id: 9, name: "Шансон"},
+    {id: 10, name: "Классическая музыка"},
+    {id: 11, name: "Рэп и Хип-Хоп"},
+    {id: 12, name: "Русская музыка"},
+    {id: 13, name: "Джаз/Блюз"},
+    {id: 14, name: "Романтика"},
+    {id: 15, name: "Юмор"},
+    {id: 16, name: "Фолк/Этно"},
+    {id: 17, name: "Детская музыка"},
+    {id: 18, name: "Альтернативная музыка/инди"},
 ]
 
 
 const StationFilter = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleCheckboxChange = (item: string) => {
+        if (selectedItems.includes(item)) {
+            setSelectedItems(selectedItems.filter((selectedItem) => selectedItem !== item));
+        } else {
+            setSelectedItems([...selectedItems, item]);
+        }
+    };
+
+    const dropdownContent = isOpen && (
+        <div
+            className={"dark:bg-gray-800 dark:text-white bg-white text-gray-800 absolute mt-4 py-2 w-48 border border-gray-300 rounded-lg shadow-lg z-10"}
+        >
+            <div className="px-4 py-2">
+                <label className="block text-sm font-medium">Filter Options</label>
+            </div>
+            <div className="px-4 py-2 space-y-1">
+                {genres.map(({id, name}) => {
+                    return                 <label key={id} className="flex items-center">
+                        <input
+                            type="checkbox"
+                            className="form-checkbox h-5 w-5 text-blue-500"
+                            value="Option 1"
+                            checked={selectedItems.includes('Option 1')}
+                            onChange={() => handleCheckboxChange('Option 1')}
+                        />
+                        <span className="ml-2">{name}</span>
+                    </label>
+                })}
+            </div>
+        </div>
+    );
+
     return (
-        <div className="mb-10 mt-5 flex flex-wrap justify-start fixed">
-            {genres.map(({ id, name }) => <FilterItem key={id} name={name}/> )}
+        <div className="relative inline-block text-left">
+            <button
+                type="button"
+                className={`dark:bg-gray-800 dark:text-white bg-white text-gray-800 px-4 py-2 rounded-lg shadow-md focus:outline-none`}
+                onClick={toggleDropdown}
+            >
+                Filter
+            </button>
+            {dropdownContent}
         </div>
     );
 };
--- app/components/layout/footer/footer.tsx
+++ app/components/layout/footer/footer.tsx
@@ -6,36 +6,42 @@ import useAudio from "@/app/hooks/useAudio";
 function Footer() {
     const currentStationId = useAppSelector((store) => store.stationReducer.currentStation.id)
 
-    const {playing, toggle} = useAudio(radioList.find(({ id }) => id === currentStationId)?.url ?? '');
+    const {playing, toggle} = useAudio(radioList.find(({id}) => id === currentStationId)?.url ?? '');
 
     return (
         <footer className="fixed inset-x-0 bottom-0 bg-no-repeat dark:bg-slate-900/75">
-                <div className="shadow-lg rounded-lg">
-                    <div className="w-full p-6">
-                        <div className="flex justify-between items-center">
-                            <div className="hover:text-sky-500 dark:hover:text-sky-400">
-                                <h3 className="text-2xl font-medium">A Sky Full of Stars</h3>
-                                <p className="text-sm mt-1">Ghost Stories</p>
-                            </div>
-                            <div className="p-8 rounded-full bg-red-light shadow-lg cursor-pointer hover:text-sky-500 dark:hover:text-sky-400" onClick={toggle}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    {playing ?
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+            <div className="shadow-lg rounded-lg">
+                <div className="w-full p-6">
+                    <div
+                        className="grid grid-cols-2 items-center justify-items-center content-center md:grid-cols-3 h-full">
+                        <div className="hover:text-sky-500 dark:hover:text-sky-400">
+                            <h3 className="text-2xl font-medium">A Sky Full of Stars</h3>
+                            <p className="text-sm mt-1">Ghost Stories</p>
+                        </div>
+                        <div
+                            className="justify-self-center p-8 rounded-full bg-red-light shadow-lg cursor-pointer hover:text-sky-500 dark:hover:text-sky-400"
+                            onClick={toggle}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="w-6 h-6">
+                                {playing ?
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"/>
                                     :
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-                                    }
-                                </svg>
-                            </div>
-                            <div className="hover:text-sky-500 dark:hover:text-sky-400" >
-                                <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20">
-                                    <path
-                                        d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"/>
-                                </svg>
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="M15.75 5.25v13.5m-7.5-13.5v13.5"/>
+                                }
+                            </svg>
+                        </div>
+                        <div className="hover:text-sky-500 dark:hover:text-sky-400">
+                            <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 20 20">
+                                <path
+                                    d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"/>
+                            </svg>
 
-                            </div>
                         </div>
                     </div>
+                </div>
             </div>
         </footer>
     );
