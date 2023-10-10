8694fb9d443ac5e9e2b6cec14f484da762cf94cc
andrey-pc <anbalabanovkz@gmail.com>
2023-10-10 20:16:37 +0300

    fix(stations): add transition

--- app/components/common/stations/station/Station.tsx
+++ app/components/common/stations/station/Station.tsx
@@ -11,10 +11,10 @@ export const Station = ({ name, stationId, currentStationId }: Props) => {
   return (
     <div
       data-id={stationId}
-      className={`block rounded-lg shadow-2xl text-center  ${
+      className={`block rounded-lg shadow-2xl text-center transition duration-600 ease-in-out  ${
         currentStationId === stationId
-          ? "bg-indigo-200 dark:bg-indigo-500"
-          : "bg-white dark:bg-slate-800"
+          ? "bg-indigo-300 dark:bg-indigo-800"
+          : "bg-white dark:bg-slate-800 hover:bg-indigo-100 hover:dark:bg-indigo-700"
       } cursor-pointer`}
     >
       <div className="p-6">LOGO</div>
